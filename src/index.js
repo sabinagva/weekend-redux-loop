64438a30e84cd317b553ce39e3479bc98d830fba
Sabina Gahramanova <sabina.ghrmnli@gmail.com>
2023-06-10 18:58:12 -0500

    set up reducers,provider and store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,49 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
+//importing redux
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import { createStore, combineReducers, applyMiddleware} from 'redux';
+
+//reducers
+const feelingsReducer = (state = [], action) => {
+    return state;
+}
+const commentsReducer = (state = [], action) => {
+    return state;
+}
+const understandingReducer = (state = [], action) => {
+    return state;
+}
+
+const supportReducer = (state = [], action) => {
+    return state;
+}
+
+const feedbackResultReducer = (state = [], action) => {
+    return state;
+}
+
+//store
+const reduxStore = createStore(
+    combineReducers({
+        feedbackResultReducer,
+        feelingsReducer,
+        commentsReducer,
+        understandingReducer,
+        supportReducer
+
+    }).applyMiddleware(
+        logger
+)
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
+        <Provider store = {reduxStore}>
         <App />
+        </Provider>
     </React.StrictMode>
 );
